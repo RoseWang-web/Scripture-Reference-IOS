37ba5a8145f3c5c1cefa3793767f6c9abd457f7b
sethchens <bryan31032@gmail.com>
2025-10-25 14:20:28 -0600

    fixed conflic

--- scripture-streamer-backend/src/scriptures/scriptures.module.ts
+++ scripture-streamer-backend/src/scriptures/scriptures.module.ts
@@ -1,10 +1,10 @@
-import { Module } from '@nestjs/common';
-import { ScripturesService } from './scriptures.service';
-import { ScriptureController } from './scriptures.controller';
+// import { Module } from '@nestjs/common';
+// import { ScripturesService } from './scriptures.service';
+// import { ScriptureController } from './scriptures.controller';
 
-@Module({
-    providers: [ScripturesService],
-    controllers: [ScriptureController],
-    exports: [ScripturesService],
-})
-export class ScripturesModule { }
+// @Module({
+//     providers: [ScripturesService],
+//     controllers: [ScriptureController],
+//     exports: [ScripturesService],
+// })
+// export class ScripturesModule { }
