35fd81aa45948da6b2075c77bd36fc2205565920
chengensung1219 <156931175+chengensung1219@users.noreply.github.com>
2025-10-24 19:26:45 -0600

    Initial basic structure setup

---
+++ Scripture-Reference-iOS/Scripture-Reference-iOS/Scripture_Reference_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Scripture_Reference_iOSApp.swift
+//  Scripture-Reference-iOS
+//
+//  Created by Simon Sung on 10/24/25.
+//
+
+import SwiftUI
+
+@main
+struct Scripture_Reference_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
